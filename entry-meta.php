8926ff94be395113635b0c3918a17de4a58df1ed
Chris Drinkut <chrisdrinkut@gmail.com>
2019-07-13 14:30:22 -0400

    css
    style name updates; bem

--- entry-meta.php
+++ entry-meta.php
@@ -1,5 +1,5 @@
-<section class="entry-meta">
-<span class="author vcard"><?php the_author_posts_link(); ?></span>
-<span class="meta-sep"> | </span>
-<span class="entry-date"><?php the_time( get_option( 'date_format' ) ); ?></span>
+<section class="entry__meta">
+<span class="entry__author"><?php the_author_posts_link(); ?></span>
+<span class="meta__sep"> | </span>
+<span class="entry__date"><?php the_time( get_option( 'date_format' ) ); ?></span>
 </section>
