c1d6e88948ec5205662969137e8b3c5c56ebd51b
Chris Drinkut <chrisdrinkut@gmail.com>
2019-07-13 14:39:19 -0400

    various
    - renaming of selectors, removal of extras

--- header.php
+++ header.php
@@ -6,18 +6,17 @@
 <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
 <?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?>>
-<div id="wrapper" class="hfeed">
-<header id="header" role="banner">
-<section id="branding">
-<div id="site-title"><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?></div>
-<div id="site-description"><?php bloginfo( 'description' ); ?></div>
+<body>
+<header class="header" role="banner">
+<section class="branding">
+<div class="site-title"><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?></div>
+<div class="site-description"><?php bloginfo( 'description' ); ?></div>
 </section>
-<nav id="menu" role="navigation">
-<div id="search">
+<nav class="menu--main" role="navigation">
+<div class="search__form">
 <?php get_search_form(); ?>
 </div>
 <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
 </nav>
 </header>
-<div id="container">
+<div id="page">
