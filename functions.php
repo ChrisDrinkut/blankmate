6bc784900487496d2c3d54396be2ea151383e6b3
Chris Drinkut <chrisdrinkut@gmail.com>
2019-07-14 10:23:20 -0400

    remove wp_head extras

--- functions.php
+++ functions.php
@@ -65,3 +65,69 @@ return count( $comments_by_type['comment'] );
 return $count;
 }
 }
+
+
+
+
+remove_action('wp_head', 'rsd_link'); // remove really simple discovery link
+remove_action('wp_head', 'wp_generator'); // remove wordpress version
+remove_action('wp_head', 'feed_links', 2); // remove rss feed links (make sure you add them in yourself if youre using feedblitz or an rss service)
+remove_action('wp_head', 'feed_links_extra', 3); // removes all extra rss feed links
+remove_action('wp_head', 'index_rel_link'); // remove link to index page
+remove_action('wp_head', 'wlwmanifest_link'); // remove wlwmanifest.xml (needed to support windows live writer)
+remove_action('wp_head', 'start_post_rel_link', 10, 0); // remove random post link
+remove_action('wp_head', 'parent_post_rel_link', 10, 0); // remove parent post link
+remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0); // remove the next and previous post links
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+      
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+      
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0); // Remove shortlink
+
+
+add_action('get_header', 'remove_admin_login_header');
+function remove_admin_login_header() {
+	remove_action('wp_head', '_admin_bar_bump_cb');
+}
+
+/*
+* Remove JSON API links in header html
+*/
+function remove_json_api () {
+    // Remove the REST API lines from the HTML Header
+    remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+    remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
+    // Remove the REST API endpoint.
+    remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+    // Turn off oEmbed auto discovery.
+    add_filter( 'embed_oembed_discover', '__return_false' );
+    // Don't filter oEmbed results.
+    remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
+    // Remove oEmbed discovery links.
+    remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+    // Remove oEmbed-specific JavaScript from the front-end and back-end.
+    remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+   // Remove all embeds rewrite rules.
+   add_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
+}
+add_action( 'after_setup_theme', 'remove_json_api' );
+/*
+	Snippet completely disable the REST API and shows {"code":"rest_disabled","message":"The REST API is disabled on this site."} 
+	when visiting http://yoursite.com/wp-json/
+*/
+function disable_json_api () {
+  // Filters for WP-API version 1.x
+  add_filter('json_enabled', '__return_false');
+  add_filter('json_jsonp_enabled', '__return_false');
+  // Filters for WP-API version 2.x
+  add_filter('rest_enabled', '__return_false');
+  add_filter('rest_jsonp_enabled', '__return_false');
+}
+add_action( 'after_setup_theme', 'disable_json_api' );
+
+function remove_recent_comments_style() {
+    global $wp_widget_factory;
+    remove_action('wp_head', array($wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));
+}
+add_action('widgets_init', 'remove_recent_comments_style');
